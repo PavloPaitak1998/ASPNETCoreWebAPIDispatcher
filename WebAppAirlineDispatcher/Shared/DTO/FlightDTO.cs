f11cfa9cbadc5ba781f0258704a3512f7f9e93c6
PavloPaitak <38991419+PavloPaitak@users.noreply.github.com>
2018-07-11 18:15:54 +0300

    Created FlightDTO

---
+++ WebAppAirlineDispatcher/Shared/DTO/FlightDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Shared.DTO
+{
+    public sealed class FlightDTO
+    {
+        public int Number { get; set; }
+        public string PointOfDeparture { get; set; }
+        public DateTime DepartureTime { get; set; }
+        public string Destination { get; set; }
+        public DateTime DestinationTime { get; set; }
+        public int TicketId { get; set; }
+    }
+}
