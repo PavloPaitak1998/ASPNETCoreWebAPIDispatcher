68db5ba23b964c5a692114653910c27eb8cb32b4
PavloPaitak <38991419+PavloPaitak@users.noreply.github.com>
2018-07-11 17:29:40 +0300

    Created DataSource

---
+++ WebAppAirlineDispatcher/DataAccessLayer/Interfaces/IDataSource.cs
@@ -0,0 +1,19 @@
+﻿using DataAccessLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccessLayer.Interfaces
+{
+    public interface IDataSource
+    {
+        List<Flight> FlightList { get; set; }
+        List<Ticket> TicketList { get; set; }
+        List<Departure> DepartureList { get; set; }
+        List<Stewardess> StewardessList { get; set; }
+        List<Pilot> PilotList { get; set; }
+        List<Crew> CrewList { get; set; }
+        List<Plane> PlaneList { get; set; }
+        List<PlaneType> PlaneTypeList { get; set; }
+    }
+}
