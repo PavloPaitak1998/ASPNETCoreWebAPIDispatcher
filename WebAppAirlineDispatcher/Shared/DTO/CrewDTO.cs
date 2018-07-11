daaae51c35708a0ac76750ac8970bf2542526a3f
PavloPaitak <38991419+PavloPaitak@users.noreply.github.com>
2018-07-11 18:14:03 +0300

    Created Shared project and class CrewDTO

---
+++ WebAppAirlineDispatcher/Shared/DTO/CrewDTO.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Shared.DTO
+{
+    public sealed class CrewDTO
+    {
+        public int Id { get; set; }
+        public int PilotId { get; set; }
+        public int[] StewardessesId { get; set; }
+    }
+}
