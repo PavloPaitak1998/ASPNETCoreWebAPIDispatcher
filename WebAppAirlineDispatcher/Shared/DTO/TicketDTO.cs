8fbfaec704c31b1509a2b05e04f67c9a24e1021b
PavloPaitak <38991419+PavloPaitak@users.noreply.github.com>
2018-07-11 18:19:56 +0300

    Created TicketDTO

---
+++ WebAppAirlineDispatcher/Shared/DTO/TicketDTO.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Shared.DTO
+{
+    public sealed class TicketDTO
+    {
+        public int Id { get; set; }
+        public double Price { get; set; }
+        public int FlightNumber { get; set; }
+    }
+}
