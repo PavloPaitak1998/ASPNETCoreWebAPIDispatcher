50dcf0d752deef1a0229741e0f2bbec981aca2ec
PavloPaitak <38991419+PavloPaitak@users.noreply.github.com>
2018-07-11 17:03:50 +0300

    Created IRepository

---
+++ WebAppAirlineDispatcher/DataAccessLayer/Interfaces/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataAccessLayer.Interfaces
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        TEntity Get(int id);
+        IEnumerable<TEntity> GetAll();
+        IEnumerable<TEntity> Find(Func<TEntity, Boolean> predicate);
+        void Create(TEntity entity);
+        void Update(TEntity entity);
+        void Delete(int id);
+        void DeleteAll();
+
+    }
+}
