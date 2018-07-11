57d9e663fadc6d8fd3be03bb95399f871d00dddf
PavloPaitak <38991419+PavloPaitak@users.noreply.github.com>
2018-07-11 17:53:45 +0300

    Created FlightRepository

--- WebAppAirlineDispatcher/DataAccessLayer/Data/DataSource.cs
+++ WebAppAirlineDispatcher/DataAccessLayer/Data/DataSource.cs
@@ -2,7 +2,6 @@
 using DataAccessLayer.Models;
 using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace DataAccessLayer.Data
 {
---
+++ WebAppAirlineDispatcher/DataAccessLayer/Implementation/Repositories/FlightRepository.cs
@@ -0,0 +1,68 @@
+﻿using DataAccessLayer.Interfaces;
+using DataAccessLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DataAccessLayer.Implementation.Repositories
+{
+    public class FlightRepository : IRepository<Flight>
+    {
+        IDataSource dataSource;
+
+        public FlightRepository(IDataSource data)
+        {
+            dataSource = data;
+        }
+
+        public Flight Get(int number)
+        {
+            return dataSource.FlightList.Find(f => f.Number == number);
+        }
+
+        public IEnumerable<Flight> GetAll()
+        {
+            return dataSource.FlightList;
+        }
+
+        public void Create(Flight entity)
+        {
+            dataSource.FlightList.Add(entity);
+        }
+
+        public IEnumerable<Flight> Find(Func<Flight, bool> predicate)
+        {
+            return dataSource.FlightList.Where(predicate);
+        }
+
+        public void Update(Flight entity)
+        {
+            var flight = dataSource.FlightList.Find(f => f.Number == entity.Number);
+
+            if (entity.DepartureTime != null)
+                flight.DepartureTime = entity.DepartureTime;
+            if (entity.PointOfDeparture != null)
+                flight.PointOfDeparture = entity.PointOfDeparture;
+            if (entity.Destination != null)
+                flight.Destination = entity.Destination;
+            if (entity.DestinationTime != null)
+                flight.DestinationTime = entity.DestinationTime;
+            if (entity.TicketsId != null)
+                flight.TicketsId = entity.TicketsId;
+        }
+
+        public void Delete(int number)
+        {
+            var flights = dataSource.FlightList;
+
+            flights.Remove(flights.Find(f=>f.Number==number));
+        }
+
+        public void DeleteAll()
+        {
+            var flights = dataSource.FlightList;
+
+            flights.RemoveRange(0, flights.Count());
+        }
+    }
+}
