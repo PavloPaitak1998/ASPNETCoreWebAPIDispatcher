68c4cdcf96c20bd5cbcd348ee1ec76050a785dab
PavloPaitak <38991419+PavloPaitak@users.noreply.github.com>
2018-07-11 17:12:55 +0300

    Created Models Ticket

---
+++ WebAppAirlineDispatcher/DataAccessLayer/Models/Ticket.cs
@@ -0,0 +1,10 @@
+﻿
+namespace DataAccessLayer.Models
+{
+    public sealed class Ticket
+    {
+        public int Id { get; set; }
+        public double Price { get; set; }
+        public int FlightNumber { get; set; }
+    }
+}
