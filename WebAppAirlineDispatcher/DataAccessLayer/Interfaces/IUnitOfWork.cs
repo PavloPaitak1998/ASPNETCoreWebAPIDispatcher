bf0be5593c140164b656b698cad729d9d839f577
PavloPaitak <38991419+PavloPaitak@users.noreply.github.com>
2018-07-11 17:14:44 +0300

    Created IUnitOfWork

--- WebAppAirlineDispatcher/DataAccessLayer/Interfaces/IUnitOfWork.cs
+++ WebAppAirlineDispatcher/DataAccessLayer/Interfaces/IUnitOfWork.cs
@@ -1,10 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using DataAccessLayer.Models;
+using System;
+using System.Threading.Tasks;
 
 namespace DataAccessLayer.Interfaces
 {
     public interface IUnitOfWork : IDisposable
     {
+        IRepository<Flight> Flights { get; }
+        IRepository<Ticket> Tickets { get; }
+        IRepository<Departure> Departures { get; }
+        IRepository<Stewardess> Stewardesses { get; }
+        IRepository<Pilot> Pilots { get; }
+        IRepository<Crew> Crew { get; }
+        IRepository<Plane> Planes { get; }
+        IRepository<PlaneType> PlaneTypes { get; }
+
+        int SaveChanges();
+        Task<int> SaveChangesAsync();
     }
 }
