ff668e9c461c6b397863c10f92b9ae0399c79b36
PavloPaitak <38991419+PavloPaitak@users.noreply.github.com>
2018-07-11 17:06:16 +0300

    Created Model Crew

---
+++ WebAppAirlineDispatcher/DataAccessLayer/Models/Crew.cs
@@ -0,0 +1,10 @@
+﻿
+namespace DataAccessLayer.Models
+{
+    public sealed class Crew
+    {
+        public int Id { get; set; }
+        public int PilotId { get; set; }
+        public int[] StewardessesId { get; set; }
+    }
+}
