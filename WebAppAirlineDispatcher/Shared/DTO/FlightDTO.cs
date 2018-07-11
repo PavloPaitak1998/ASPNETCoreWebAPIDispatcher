436ba8ca12ae2e9893ed3570dcdabf7d11a5c4ec
PavloPaitak <38991419+PavloPaitak@users.noreply.github.com>
2018-07-11 18:17:11 +0300

    Created PilotDTO

--- WebAppAirlineDispatcher/Shared/DTO/FlightDTO.cs
+++ WebAppAirlineDispatcher/Shared/DTO/FlightDTO.cs
@@ -9,6 +9,6 @@ namespace Shared.DTO
         public DateTime DepartureTime { get; set; }
         public string Destination { get; set; }
         public DateTime DestinationTime { get; set; }
-        public int TicketId { get; set; }
+        public int[] TicketsId { get; set; }
     }
 }
