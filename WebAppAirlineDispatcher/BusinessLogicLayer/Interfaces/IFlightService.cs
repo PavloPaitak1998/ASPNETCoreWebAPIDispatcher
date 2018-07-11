5dab4271d0c76a76d6c0cc5081ac20f8853180db
PavloPaitak <38991419+PavloPaitak@users.noreply.github.com>
2018-07-11 18:32:25 +0300

    Created IFlightService in BLL

---
+++ WebAppAirlineDispatcher/BusinessLogicLayer/Interfaces/IFlightService.cs
@@ -0,0 +1,18 @@
+﻿using Shared.DTO;
+using System;
+using System.Collections.Generic;
+
+namespace BusinessLogicLayer.Interfaces
+{
+    public interface IFlightService
+    {
+        FlightDTO GetFlight(int? id);
+        IEnumerable<FlightDTO> GetFlights();
+        void AddFlight(FlightDTO flightDTO);
+        IEnumerable<FlightDTO> FindFlights(Func<FlightDTO, bool> predicate);
+        void CreateFlight(FlightDTO flightDTO);
+        void UpdateFlight(FlightDTO flightDTO);
+        void DeleteFlight(int? id);
+        void DeleteAllFlights();
+    }
+}
