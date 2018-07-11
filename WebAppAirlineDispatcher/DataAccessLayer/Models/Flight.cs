49dff99ec1ef2fee146d9f36b18148a0bc263c59
PavloPaitak <38991419+PavloPaitak@users.noreply.github.com>
2018-07-11 17:08:53 +0300

    Created Model Flight

---
+++ WebAppAirlineDispatcher/DataAccessLayer/Models/Flight.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DataAccessLayer.Models
+{
+    public sealed class Flight
+    {
+        public int Number { get; set; }
+        public string PointOfDeparture { get; set; }
+        public DateTime DepartureTime { get; set; }
+        public string Destination { get; set; }
+        public DateTime DestinationTime { get; set; }
+        public int[] TicketsId { get; set; }
+    }
+}
